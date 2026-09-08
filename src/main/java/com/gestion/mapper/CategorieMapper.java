e085efcdcb53b0ac897430fc29612e4cfb46d3a1
adilerrai <adilerrai24@gmail.com>
2026-09-08 16:22:05 +0100

    aaaa

--- src/main/java/com/gestion/mapper/CategorieMapper.java
+++ src/main/java/com/gestion/mapper/CategorieMapper.java
@@ -3,11 +3,15 @@ package com.gestion.mapper;
 import com.gestion.persistent.dto.CategorieDTO;
 import com.gestion.persistent.model.Categorie;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface CategorieMapper {
 
+    @Mapping(source = "parent.id", target = "parentId")
+    @Mapping(source = "parent.nom", target = "parentNom")
     CategorieDTO toDto(Categorie categorie);
 
+    @Mapping(source = "parentId", target = "parent.id")
     Categorie toEntity(CategorieDTO categorieDTO);
 }
